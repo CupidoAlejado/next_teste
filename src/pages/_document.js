b12eeb81a43128a41671aa446fb60232db62efc2
Luis Fernano <149402596+CupidoAlejado@users.noreply.github.com>
2025-04-23 15:29:00 +0000

    YESSIR

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,12 +1,16 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import  Menu  from "./componentes/menu.js";
+import  Footer  from "./componentes/footer.js";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <Menu />
         <Main />
         <NextScript />
+        <Footer />
       </body>
     </Html>
   );
