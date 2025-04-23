52e03f984cbae94526e6f9d065913104183a8398
Luis Fernano <149402596+CupidoAlejado@users.noreply.github.com>
2025-04-23 15:34:11 +0000

    COMITEIIII

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import  Menu  from "./componentes/menu.js";
 import  Footer  from "./componentes/footer.js";
+import  Esportes  from "./esportes.js";
 
 export default function Document() {
   return (
